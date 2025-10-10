2f47d2e25ee70eb1946e0a7d0d77186efdfb826d
Daniil Mukhreev <muxreev.dan@yandex.ru>
2025-10-10 23:04:51 +0300

    02_homework_update

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import "console-emojis"
-import { cristal, summer } from "gradient-string"
+import { cristal, summer, vice } from "gradient-string"
 import chalk from "chalk"
 
 import lessonTwo from "./02_lesson/index.js"
+import lessonThree from "./03_lesson/index.js"
 
 console.handshake(cristal("Всем привет!\n"))
 
+console.one(chalk.bold(vice("Введение в JavaScript. Переменные и типы данных")))
+// lessonTwo()
+
 console.two(chalk.bold(summer(" Преобразование типов. Операторы сравнения")))
-lessonTwo()
+lessonThree()
